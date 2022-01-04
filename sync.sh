ab004fa9b0f9043cdd31ddcdbf704c31d8a50a0b
shinbeth <viktoras@prodenta.lt>
2022-01-04 21:25:40 +0200

    "$KOMENTARAS"

--- sync.sh
+++ sync.sh
@@ -2,5 +2,5 @@
 KOMENTARAS = $(/usr/bin/date --rfc-3339=seconds)
 
 /usr/bin/git add --all ./* \
-&& /usr/bin/git commit -m '$KOMENTARAS' \
+&& /usr/bin/git commit -m '"$KOMENTARAS"' \
 && /usr/bin/git push;
