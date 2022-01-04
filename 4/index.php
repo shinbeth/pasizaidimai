f22e497157389fe30878001f6ae9c6f1f76ad510
shinbeth <viktoras@prodenta.lt>
2022-01-04 19:41:05 +0200

    ketvirta pamoka1

--- 4/index.php
+++ 4/index.php
@@ -1,3 +1,35 @@
 <?php 
 
-echo '4 pamoka';
+//echo '4 pamoka';
+
+$productPrice = 12;
+$discout = 20;
+
+$finalPrice = $productPrice * ((100 - $discount) / 100);
+
+//echo '<div class="price">'.$finalPrice.'</div>';
+
+
+function getDiscount($Price, $Discount){
+
+        $discountedPrice = $Price * (( 100 - $Discount ) / 100);
+    
+return $discountedPrice;
+}
+
+
+function addVat($Price, $Vat){
+    $vatRatio = $Vat / 100 + 1;
+
+    $vatedPrice = $Price * $vatRatio;
+
+    return $vatedPrice;
+}
+
+$ltVat = 21;
+
+echo getDiscount(100,20);
+echo '<br>';
+echo addVat(getDiscount(100,20),$ltVat);
+echo '<br>';
+echo getDiscount(1273,33);
