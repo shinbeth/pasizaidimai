6c3cc0eadbe401eaaaa7289cc30636708d0d7c02
shinbeth <viktoras@prodenta.lt>
2022-01-04 21:35:26 +0200

    comm

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-KOMENTARAS=$(/usr/bin/date --rfc-3339=seconds)
+#
+
+# Neismasciau kaip i kabutes iterpt ;/
+#KOMENTARAS=$(/usr/bin/date --rfc-3339=seconds)
 
 /usr/bin/git add --all ./* \
 && /usr/bin/git commit -m 'comm' \
