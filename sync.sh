d89d3b220564c4d3864aca818677a1960580d96b
shinbeth <viktoras@prodenta.lt>
2022-01-04 22:03:38 +0200

    atnaujinimas pamokos metu

--- sync.sh
+++ sync.sh
@@ -5,5 +5,5 @@
 #KOMENTARAS=$(/usr/bin/date --rfc-3339=seconds)
 
 /usr/bin/git add --all ./* \
-&& /usr/bin/git commit -m '$(/usr/bin/date --rfc-3339=seconds)' \
+&& /usr/bin/git commit -m 'atnaujinimas pamokos metu' \
 && /usr/bin/git push;
