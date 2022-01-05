7c2df2e23f92bf6abd1dea6c45fe7f17a9ebff67
shinbeth <viktoras@prodenta.lt>
2022-01-05 17:43:03 +0200

    'scriptSync20220105174303'

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-KOMENTARAS=$(/usr/bin/date +"'%Y%m%d%H%M%S'")
+KOMENTARAS=$(/usr/bin/date +"'scriptSync%Y%m%d%H%M%S'")
 
 /usr/bin/git add --all ./* \
 && /usr/bin/git commit -m $(echo $KOMENTARAS) \
