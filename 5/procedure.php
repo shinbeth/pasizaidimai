be45ad01ef769df47052f15dc7d54aee76cda12e
shinbeth <viktoras@prodenta.lt>
2022-01-05 19:49:44 +0200

    'script_sync_2022_01_05_194944'

--- 5/procedure.php
+++ 5/procedure.php
@@ -1,3 +1,23 @@
 <?php 
 
-echo 'ok';
+//echo 'ok';
+// $_GET
+//print_r($_POST);
+
+//
+
+$email = $_POST[email];
+
+function isEmailValid($email){
+
+    if(strpos($email, '@')){
+        echo $email;
+        echo '<br>';
+    } else{
+        echo 'lievas';
+        echo '<br>';
+        }
+
+}
+
+isEmailValid($email);
