a775cc634b93249ba5231ea9071998231800e468
shinbeth <viktoras@prodenta.lt>
2022-01-05 17:46:21 +0200

    'script_sync_2022_01_05_174621'

--- sync.sh
+++ sync.sh
@@ -3,6 +3,8 @@
 
 KOMENTARAS=$(/usr/bin/date +"'script_sync_%Y_%m_%d_%H%M%S'")
 
+# siuo skriptu sinchronizuoju pasizaidimu direktorija 
+
 /usr/bin/git add --all ./* \
 && /usr/bin/git commit -m $(echo $KOMENTARAS) \
 && /usr/bin/git push;
