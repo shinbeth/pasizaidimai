88636ad12cb7911d57af4e2f808b5be2712dd976
shinbeth <viktoras@prodenta.lt>
2022-01-05 17:18:24 +0200

    '010520221718'

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-KOMENTARAS=$(/usr/bin/date +"'%m%d%Y'")
+KOMENTARAS=$(/usr/bin/date +"'%m%d%Y%H%M'")
 
 /usr/bin/git add --all ./* \
 && /usr/bin/git commit -m $(echo $KOMENTARAS) \
