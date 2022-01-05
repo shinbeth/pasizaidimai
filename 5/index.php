fce77ae321780de26ab664b65df50c8f8aef9463
shinbeth <viktoras@prodenta.lt>
2022-01-05 18:51:24 +0200

    'script_sync_2022_01_05_185124'

---
+++ 5/index.php
@@ -0,0 +1,47 @@
+// ^(.+)$
+// echo ' \1';
+
+<?php
+echo ' <!DOCTYPE html>';
+
+echo ' <html>';
+echo '         <head>';
+
+echo '             <title> Antraste. Matosi narsykles tabe. </title>';
+echo '         ';
+echo '         </head>';
+echo '         ';
+echo '         <body>';
+
+echo '             <h1> Pagrindine Antraste </h1>';
+echo '             ';
+echo '                 <p> pastraipa. tekstas </p>';
+echo '             ';
+echo '             <h2> Antrine Antraste </h2>';
+echo '             ';
+echo '                 <p> dar kita pastraipa </p>';
+echo '             ';
+echo '             <h3> Tretine Antraste 1 </h3>';
+echo '             ';
+echo '                 <p> sunumeruotas sarasas </p>';
+echo '             ';
+echo '                 <ol>';
+echo '                     <li> pirmas saraso elementas </li>';
+echo '                     <li> antras saraso elementas </li>';
+echo '                 </ol>';
+echo '                 ';
+echo '             <h3> Tretine Antraste 2 </h3>';
+echo '             ';
+echo '                 <p> nesunumeruotas sarasas </p>';
+echo '             ';
+echo '                 <ul>';
+echo '                     <li> pirmas saraso elementas </li>';
+echo '                     <li> antras saraso elementas </li>';
+echo '                 </ul>';
+echo '         ';
+echo '         </body>';
+echo '         ';
+echo ' </html>';
+echo '         ';
+
+
