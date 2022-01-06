c8dda2593a63eaefd3a31c3d553eab5533cd03c4
shinbeth <viktoras@prodenta.lt>
2022-01-06 21:39:24 +0200

    'script_sync_2022_01_06_213924'

--- 6/scripts/isEmailTaken.php
+++ 6/scripts/isEmailTaken.php
@@ -22,24 +22,24 @@ $firma = $_POST['firma'];
 if (filter_var($emailas, FILTER_VALIDATE_EMAIL)) {
   //echo("$emailas is a valid email address");
 } else {
-  die("<b>$emailas</b> negalimas emailas!");
-}
+        die("<b>$emailas</b> negalimas emailas!");
+        }
 
 
 $lines =file('../data/failas.csv');   // atsidarom faila kurio pavadinimas failas.csv
 
 
 foreach($lines as $data)  // failas nuskaitomas po eilute naudojant cikla foreach
-{
+    {
 
-list($FirstName[],$LastName[],$Email[],$CName[]) = explode(',',$data); // stulpeliais sukarpo faila per kableli, po eilute
+        list($FirstName[],$LastName[],$Email[],$CName[]) = explode(',',$data); // stulpeliais sukarpo faila per kableli, po eilute
 
-}
+    }
 
 
 
 // tikrinimas pagal emaila kaip pagal unikalu identifikatoriu ar irasas egzistuoja
-   if(!in_array($emailas, $Email))
+if(!in_array($emailas, $Email))
    {
         $myfile = fopen("../data/failas.csv", "a") or die("Unable to open file!");
         
@@ -50,7 +50,8 @@ list($FirstName[],$LastName[],$Email[],$CName[]) = explode(',',$data); // stulpe
         fclose($myfile);
         die('Irasas pridetas');
 
-}else{
-
+    }else{
+    
         die('irasas jau egzistuoja');
-}
+        
+         }
