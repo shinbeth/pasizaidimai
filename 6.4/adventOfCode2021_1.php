28d2dbbc03cd29cf7d22e211e96ed3f6187b7a31
shinbeth <viktoras@prodenta.lt>
2022-01-09 23:34:00 +0200

    'script_sync_2022_01_09_233400'

--- 6.4/adventOfCode2021_1.php
+++ 6.4/adventOfCode2021_1.php
@@ -2,22 +2,17 @@
 
 $data = file('data/input.log');
 
-
-//var_dump($data);
-
 $data_length = count($data);
 
-$increaseC=0; // pirmu spejimu prasukus cikla gavau per maza skaiciu
-// pasistumdziau su offsetu ciklo ir vienintele mintis
+$increaseC=0; 
 
-for($i=1;$i<$data_length;$i++){
 
-    
+for($i=0;$i<$data_length;$i++){
+
+
           if($data[$i]>$data[$i-1]==true){
-            //echo $data[$i].'>'.$data[$i-1];
                 $increaseC++;
-          }
-    
-
+          }    
 }
 
+echo $increaseC."\n";
