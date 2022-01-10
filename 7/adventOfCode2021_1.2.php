622f3a814a8fe6343d78667365299502c2960942
shinbeth <viktoras@prodenta.lt>
2022-01-10 18:34:16 +0200

    'script_sync_2022_01_10_183416'

---
+++ 7/adventOfCode2021_1.2.php
@@ -0,0 +1,13 @@
+<?php
+$data = file('../6.4/data/input.log');
+
+$data_length = count($data)-2;
+
+$increaseC=0; 
+
+for($i=0;$i<=$data_length;$i++){
+          if(($data[$i]+$data[$i+1]+$data[$i+2])<($data[$i+1]+$data[$i+2]+$data[$i+3])){
+                $increaseC++;
+          }    
+}
+echo $increaseC."\n";
