edd9ff9c75e3ce8382f0ab15a0a204534c1b2bca
shinbeth <viktoras@prodenta.lt>
2022-01-10 20:28:34 +0200

    'script_sync_2022_01_10_202834'

--- 7.1/adventOfCode2021_1.2.php
+++ 7.1/adventOfCode2021_1.2.php
@@ -9,7 +9,7 @@ $data_length = count($data)-4;
 $increaseC=0; 
 
 for($i=0;$i<=$data_length;$i++){
-          if(($data[$i]+$data[$i+1]+$data[$i+2])<($data[$i+1]+$data[$i+2]+$data[$i+3])){
+          if(((int)$data[$i]+(int)$data[$i+1]+(int)$data[$i+2])<((int)$data[$i+1]+(int)$data[$i+2]+(int)$data[$i+3])){
                 $increaseC++;
           }    
 }
