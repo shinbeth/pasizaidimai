661de67fefba3b8c9a8393dc5f6fe6e149380fe3
shinbeth <viktoras@prodenta.lt>
2022-01-11 11:12:51 +0200

    'script_sync_2022_01_11_111251'

--- 7.4/currency.sh
+++ 7.4/currency.sh
@@ -2,5 +2,3 @@ lynx -source https://rekvizitai.vz.lt/valiutu-kursai/ > cur_valiutos.html
 
 
 egrep "curRates\['[A-Z]{2,3}'\] = [0-9]{1,2}\.[0-9]{10}" cur_valiutos.html > temp_cur_valiutos.list
-
-<option value="RUB" title="Rusijos rublis">RUB</option>
