10de4467fe692f5cee3bd0bca5878af6239f3456
shinbeth <viktoras@prodenta.lt>
2022-01-11 12:14:53 +0200

    'script_sync_2022_01_11_121453'

--- 7.4/currency.sh
+++ 7.4/currency.sh
@@ -1,4 +1,1 @@
-lynx -source https://rekvizitai.vz.lt/valiutu-kursai/ > cur_valiutos.html
-
-
-egrep "curRates\['[A-Z]{2,3}'\] = [0-9]{1,2}\.[0-9]{10}" cur_valiutos.html > temp_cur_valiutos.list
+/usr/bin/lynx -source https://rekvizitai.vz.lt/valiutu-kursai/ | /usr/bin/egrep "curRates\['[A-Z]{2,3}'\] = [0-9]{1,2}\.[0-9]{10}" > temp_cur_valiutos.list
